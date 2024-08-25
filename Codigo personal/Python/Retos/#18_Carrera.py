7d485672c6d92990a9904c1a0cbd0f440226d762
Zake207 <airampg207@gmail.com>
2024-08-25 13:45:49 +0100

    Obsidian-update

--- Codigo personal/Python/Retos/#18_Carrera.py
+++ Codigo personal/Python/Retos/#18_Carrera.py
@@ -15,3 +15,45 @@
  * Para ello tiene que realizar la opción correcta en cada tramo de la pista.
 """
 
+def race_analiser(runner_str, field_str) -> bool:
+    success = True
+    race_str = ""
+    if (len(runner_str) != len(field_str)) :
+        print("The strings passed are not the same lenght.")
+        return False
+    print ("Evaluating the string passed by parameter...")
+    for element in range(0, len(field_str)) :
+        match runner_str[element] :
+            case "r" :
+                if (field_str[element] == "_") :
+                    race_str += "_"
+                elif (field_str[element] == "|") :
+                    race_str += "/"
+                    success = False
+                else :
+                    race_str += "?"
+                    success = False
+            case "j" :
+                if (field_str[element] == "_") :
+                    race_str += "x"
+                    success = False
+                elif (field_str[element] == "|") :
+                    race_str += "|"
+                else :
+                    race_str += "?"
+                    success = False
+            case _:
+                race_str += "!"
+                success = False
+    print(race_str)
+    return success
+        
+runner = "rjmmrjrj"
+field = "_||_|_ab"
+
+print (f"The runner string: {runner}")
+print(f"The field string:  {field}")
+if race_analiser(runner, field) :
+    print("Race was a success")
+else :
+    print("Race was a fiasco")
