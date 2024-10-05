28d62419c246a668e51fedb31849b174cfd5b695
Zake207 <airampg207@gmail.com>
2024-10-05 17:54:58 +0100

    05/10/24

--- Practica_1_IA/lib/Nodo.h
+++ Practica_1_IA/lib/Nodo.h
@@ -13,50 +13,32 @@ using namespace std;
 
 class Nodo {
 public:
-    Nodo(int id = -1, const vector<pair<int, double>> &children = vector<pair<int, double>>(), const vector<pair<int, double>> &parents = vector<pair<int, double>>()) {
-        id_ = id;
-        children_;
-        parents_;
-    }
-
-    int getId() {
-        return id_;
-    }
-
-    vector<pair<int, double>> &getChildren() {
-        return children_;
-    }
-
-    vector<pair<int, double>> &getParents() {
-        return parents_;
-    }
-
-    void InsertChild(int id, double cost) {
-        pair<int, double> child = {id, cost};
-        children_.push_back(child);
-    }
-
-    void InsertParent(int id, double cost) {
-        pair<int, double> parent = {id, cost};
-        parents_.push_back(parent);
-    }
-
-    // void RemoveChild(int id) {
-    //     children_.erase({id, 0});
-    // }
-    friend ostream &operator<<(ostream &os, const Nodo &nodo) {
-        os << "ID: " << nodo.id_ + 1 << endl;
-        for (auto &child : nodo.children_) {
-            os << " -> " << child.first + 1 << " (" << child.second << ")" << endl;
-        }
-        for (auto &parent : nodo.parents_) {
-            os << " <- " << parent.first + 1<< " (" << parent.second << ")";
-        }
-        return os;
-    }
-
-
-
+    /// @brief Constructor de la clase Nodo
+    /// @param id identificador del nodo
+    /// @param children Los hijos del nodo
+    /// @param parents Los padres del nodo
+    Nodo(int id = -1, const vector<pair<int, double>> &children = vector<pair<int, double>>(), const vector<pair<int, double>> &parents = vector<pair<int, double>>());
+    /// @brief Getter del identificador del nodo
+    /// @return El identificador del nodo
+    int getId() {return id_;}
+    /// @brief Getter de los hijos del nodo
+    /// @return Los hijos del nodo
+    vector<pair<int, double>> &getChildren() {return children_;}
+    /// @brief Getter de los hijos del nodo
+    /// @return Los hijos del nodo
+    vector<pair<int, double>> &getParents() {return parents_;}
+    /// @brief Inserta un hijo al nodo
+    /// @param id identificador del hijo
+    /// @param cost coste del hijo
+    void InsertChild(int id, double cost);
+    /// @brief Inserta un padre al nodo
+    /// @param id identificador del padre
+    /// @param cost coste del padre
+    void InsertParent(int id, double cost);
+    /// @brief Sobrecarga del operador de inserción
+    /// @param os flujo de salida
+    /// @param nodo nodo a imprimir
+    friend ostream &operator<<(ostream &os, const Nodo &nodo);
 
 private:
     int id_;
